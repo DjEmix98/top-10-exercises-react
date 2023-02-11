3f2741cc3832e9b2d0f40064e5e5ca20b64b9a80
Emmanuel-Bellissimo <118261948+Emmanuel-Bellissimo@users.noreply.github.com>
2023-02-11 17:57:09 +0100

    add title to page

--- src/pages/ImageSliderPage.js
+++ src/pages/ImageSliderPage.js
@@ -1,7 +1,18 @@
 import { ImageSlider } from "../components/ImageSlider";
 export function ImageSliderPage() {
-  const sources = ["img_1.jpeg", "img_2.png", "img_3.png", "img_4.png", "img_6.png"];
-  return <div className="d-flex justify-content-center">
-    <ImageSlider sources={sources}></ImageSlider>
-    </div>
+  const sources = [
+    "img_1.jpeg",
+    "img_2.png",
+    "img_3.png",
+    "img_4.png",
+    "img_6.png",
+  ];
+  return (
+    <>
+      <h2 className="mb-5 mt-4">N 6 - Image slider</h2>
+      <div className="d-flex justify-content-center">
+        <ImageSlider sources={sources}></ImageSlider>
+      </div>
+    </>
+  );
 }
