b80562058c14dfbaf54fba984975960f13234b21
Emmanuel-Bellissimo <118261948+Emmanuel-Bellissimo@users.noreply.github.com>
2023-02-11 15:06:49 +0100

    remove ;

--- src/pages/ImageSliderPage.js
+++ src/pages/ImageSliderPage.js
@@ -2,6 +2,6 @@ import { ImageSlider } from "../components/ImageSlider";
 export function ImageSliderPage() {
   const sources = ["img_1.jpeg", "img_2.png", "img_3.png", "img_4.png", "img_6.png"];
   return <div className="d-flex justify-content-center">
-    <ImageSlider sources={sources}></ImageSlider>;
+    <ImageSlider sources={sources}></ImageSlider>
     </div>
 }
